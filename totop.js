f15f14e5263c4c5d664aec9efe9f6bdf3141e623
Petint <petras.balint04@gmail.com>
2022-04-02 13:30:26 +0200

    Finally fixed!

--- totop.js
+++ totop.js
@@ -1,5 +1,5 @@
 //Get the button
-let mybutton = document.getElementById("btn-back-to-top");
+//let mybutton = document.getElementById("btn-back-to-top");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
@@ -8,13 +8,11 @@ window.onscroll = function () {
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
-        mybutton.style.display = "block";
+        document.getElementById("btn-back-to-top").style.display = "block";
     } else {
-        mybutton.style.display = "none";
+        document.getElementById("btn-back-to-top").style.display = "none";
     }
 }
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
 
 function backToTop() {
     document.body.scrollTop = 0;
