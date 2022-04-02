8bc0be4c256583389f98eae4bab9e9261fe48655
Petint <petras.balint04@gmail.com>
2022-04-02 12:38:15 +0200

    Return to top doesn't work Fixes Petint/Bugatti_old_timer_project#9 Reformatted totop.js in the hope that it whould actually make a difference.

--- totop.js
+++ totop.js
@@ -3,23 +3,20 @@ let mybutton = document.getElementById("btn-back-to-top");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
-scrollFunction();
+    scrollFunction();
 };
 
 function scrollFunction() {
-    if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-    ) {
-    mybutton.style.display = "block";
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        mybutton.style.display = "block";
     } else {
-    mybutton.style.display = "none";
+        mybutton.style.display = "none";
     }
-    }
-    // When the user clicks on the button, scroll to the top of the document
-    mybutton.addEventListener("click", backToTop);
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
 
-    function backToTop() {
+function backToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
